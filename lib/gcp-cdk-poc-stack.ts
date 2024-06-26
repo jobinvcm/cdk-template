1a77845b7830420937ab7b5773c469fab2b02029
jobin mathew <jobin.mathew@flightcentre.com>
2024-06-26 23:12:19 +1000

    initial commit

--- lib/gcp-cdk-poc-stack.ts
+++ lib/gcp-cdk-poc-stack.ts
@@ -1,16 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// Import the Lambda module
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class GcpCdkPocStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define the Lambda function resource
+    const myFunction = new lambda.Function(this, "HelloWorldFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello World!'),
+          };
+        };
+      `),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GcpCdkPocQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const api = new apigateway.LambdaRestApi(this, 'HelloWorldApi', {
+      handler: myFunction,
+      proxy: false,
+    });
+
+    const helloResource = api.root.addResource('hello');
+    helloResource.addMethod('GET');
   }
+  
 }
